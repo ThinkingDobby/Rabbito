cf12f3ae5fc1db60ba7444a546d12e489887eace
ThinkingDobby <chiho1221@naver.com>
2021-07-22 21:01:46 +0900

    모바일 화면 고정 설정
    <FIN>
    모바일에서는 vh 리사이징되지 않도록 변경

--- js/fixed_height.js
+++ js/fixed_height.js
@@ -1,8 +1,10 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-window.addEventListener("resize", () => {
+if (document.body.clientWidth > 1024) {
+  window.addEventListener("resize", () => {
     console.log("resize");
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   });
+}
